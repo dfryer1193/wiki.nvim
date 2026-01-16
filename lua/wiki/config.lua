77a23ac44f7488a1f2dfce8af56bc380aa87e15f
David Fryer <dfryer1193@gmail.com>
2026-01-16 18:51:00 -0500

    Add a config setup function

--- lua/wiki/config.lua
+++ lua/wiki/config.lua
@@ -1,7 +1,17 @@
 local M = {}
 
-M.root = vim.fn.expand '~/wiki'
-M.pages_dir = M.root .. '/pages'
-M.index_file = M.root .. '/index.md'
+M.root = vim.fn.expand("~/wiki")
+M.pages_dir = M.root .. "/pages"
+M.index_file = M.root .. "/index.md"
+
+function M.setup(opts)
+	opts = opts or {}
+
+	if opts.root then
+		M.root = opts.root
+		M.pages_dir = M.root .. "/pages"
+		M.index_file = M.root .. "/index.md"
+	end
+end
 
 return M
