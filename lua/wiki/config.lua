6124a078c327a93db0b380565278031fa663adaa
David Fryer <dfryer1193@gmail.com>
2026-01-16 20:53:16 -0500

    Fix double-slash in index file

--- lua/wiki/config.lua
+++ lua/wiki/config.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.root = vim.fn.expand("~/wiki")
 M.pages_dir = M.root .. "/pages"
-M.index_file = M.root .. "/index.md"
+M.index_file = M.root .. "index.md"
 
 function M.setup(opts)
 	opts = opts or {}
