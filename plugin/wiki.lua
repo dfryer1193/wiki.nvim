b683097871865d9b96af268a5497299a0afc7d61
David Fryer <dfryer1193@gmail.com>
2026-01-16 19:49:09 -0500

    Add wiki index generation

--- plugin/wiki.lua
+++ plugin/wiki.lua
@@ -5,3 +5,8 @@ end, {})
 vim.api.nvim_create_user_command("WikiSearch", function()
 	require("wiki").search()
 end, {})
+
+vim.api.nvim_create_user_command("WikiGenerate", function()
+	require("wiki.index").generate()
+	print("Wiki index generated.")
+end, {})
